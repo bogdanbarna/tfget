9490090354fc0b591f8570c451106736d748767d
Bogdan Barna <bogdangb@protonmail.com>
2021-08-20 00:03:56 +0300

    chore(ci): add golangci-lint
    Rudimentary configuration. The linter is currently still spiting errors

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"io"
 	"net/http"
+	"context"
+	"time"
 	"bufio"
 	"strings"
 	"sort"
@@ -14,13 +16,25 @@ import (
 	"golang.org/x/net/html"
 )
 
-func UnarchiveZipFile(filepath string) {
-	log.Println("Unzipping", filepath)
-}
+const releasesUrl = "https://releases.hashicorp.com/terraform/"
+
+//func UnarchiveZipFile(filepath string) {
+//	log.Println("Unzipping", filepath)
+//}
 
 func DownloadFile(filepath string, url string) error {
+	// Handle HTTP request
+	client := &http.Client{}
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return err
+	}
+
 	// Get the data
-	resp, err := http.Get(url)
+	resp, err := client.Do(req)
 	if err != nil {
 		return err
 	}
@@ -38,8 +52,19 @@ func DownloadFile(filepath string, url string) error {
 	return err
 }
 
-func ListRemoteVersions(releasesUrl string) ([]string, error) {
-	resp, err := http.Get(releasesUrl)
+func ListRemoteVersions() []string {
+	// Handle HTTP request
+	client := &http.Client{}
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 300*time.Millisecond)
+	defer cancel()
+	req, err := http.NewRequestWithContext(ctx, "GET", releasesUrl, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Get the data
+	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -51,12 +76,14 @@ func ListRemoteVersions(releasesUrl string) ([]string, error) {
 	z := html.NewTokenizer(bufio.NewReader(resp.Body))
 	for {
 		tt := z.Next()
-		switch tt {
-		case html.ErrorToken: // Reached EOF
+		if tt == html.ErrorToken { // Reached EOF
 			// Sort from latest to oldest
 			sort.Sort(sort.Reverse(sort.StringSlice(versions)))
-			return versions, nil
-		case html.StartTagToken:
+			log.WithFields(log.Fields{
+				"versions": versions,
+			}).Info("Parsed remote versions")
+			return versions
+		} else if tt == html.StartTagToken {
 			t := z.Token()
 			// <li>
 			// 	<a href="/terraform/$version_number/">terraform_$version_number</a>
@@ -88,7 +115,7 @@ func GetVersion(versions []string) string {
 			break
 		}
 	}
-	if found_it == false {
+	if !found_it {
 		log.Fatal("Version not found ", version_number)
 	}
 	return version_number
@@ -107,7 +134,9 @@ func mkdirLocalCache(dirPath string ) string {
 
 	// dirPermissions := int(0700)
 	if _, err := os.Stat(dirPath); os.IsNotExist(err) {
-		log.Println("Directory not found, creating", dirPath)
+		log.WithFields(log.Fields{
+			"dirPath": dirPath,
+		}).Info("Directory not found, creating")
 	  mkdirErr := os.Mkdir(dirPath, 0700)
 		if mkdirErr != nil {
 			log.Fatal(mkdirErr)
@@ -118,7 +147,7 @@ func mkdirLocalCache(dirPath string ) string {
 
 func init() {
 	// Log as JSON instead of the default ASCII text
-	//log.SetFormatter(&log.JSONFormatter{})
+	log.SetFormatter(&log.JSONFormatter{})
 
 	// Output to stdout instead of the default stderr
 	log.SetOutput(os.Stdout)
@@ -140,7 +169,6 @@ func ValidateVersion(version string) bool {
 }
 
 func main() {
-	releasesUrl := "https://releases.hashicorp.com/terraform/"
 	platform := runtime.GOOS + "_" + runtime.GOARCH
 	var version_number string
 
@@ -151,10 +179,7 @@ func main() {
 		}
 		version_number = cliArgs[1]
 	} else {
-		versions, err := ListRemoteVersions(releasesUrl)
-		if err != nil {
-			log.Fatal(err)
-		}
+		versions := ListRemoteVersions()
 		version_number = GetVersion(versions)
 	}
 
@@ -164,16 +189,23 @@ func main() {
 	fullPathZip := fullPath + ".zip"
 
 	if _, err := os.Stat(fullPathZip); os.IsNotExist(err) {
-		log.Println("Downloading Terraform version", version_number)
+		log.WithFields(log.Fields{
+			"version_number": version_number,
+		}).Info("Downloading Terraform version")
 		fileUrl := releasesUrl + version_number + "/terraform_" + version_number + "_" + platform + ".zip"
 
 		downloadErr := DownloadFile(fullPathZip, fileUrl)
 		if downloadErr != nil {
 			log.Fatal(downloadErr)
 		}
-		log.Println("Downloaded " + fullPathZip)
+		log.WithFields(log.Fields{
+			"fullPathZip": fullPathZip,
+			"version_number": version_number,
+		}).Info("Downloaded Terraform version on disk")
 	} else {
-		log.Println("Version", version_number, "already exists on disk")
+		log.WithFields(log.Fields{
+			"version_number": version_number,
+		}).Info("Version already exists on disk")
 	}
 
 	//UnarchiveZipFile(fullPath)
